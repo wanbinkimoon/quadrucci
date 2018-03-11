f55249d4196e0b9b6b8a05e1921ceaa3e5aab7e3
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-03-11 03:37:18 +0100

    more control

--- src/app.js
+++ src/app.js
@@ -1,44 +1,38 @@
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 
-
 const sketch = p => {
   let canvas;
-  const radiusDefault = 800
-  let els = 50
-  let distance = 30
-  
+  const radiusDefault = 800;
+  let els = 60;
+  let distance = 30;
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(255);
-    
+    p.colorMode(p.HSB)
+    p.background(0,0,100);
     p.strokeWeight(4);
-    p.noFill()
+    p.noFill();
   };
-  
+
   p.draw = () => {
-    let radius = radiusDefault
-    p.background(255);
-    
-    if(distance > 100) {
-      distance += 1
-    } else {
-      distance -= 1
-    }
-
+    let radius = radiusDefault;
+    p.background(0,0,100);
+    distance += 1;
     p.translate(p.width / 2, p.height / 2);
-
-    let i
-    for(i = 0; i <= els; i++){
-      p.ellipse((distance * -1), (distance * -1), radius, radius)
-      p.ellipse(distance, distance, radius, radius)
-      p.ellipse(distance, (distance * -1), radius, radius)
-      p.ellipse((distance * -1), distance, radius, radius)
-      p.ellipse(0, distance, radius, radius)
-      p.ellipse(0, (distance * -1), radius, radius)
-      p.ellipse(distance, 0, radius, radius)
-      p.ellipse((distance * -1), 0, radius, radius)
-      radius = radius - (radiusDefault / els) 
+    
+    let i;
+    for (i = 0; i <= els; i++) {
+      p.stroke(p.random(360), 70, 100)
+      p.ellipse(distance * -1, distance * -1, radius, radius);
+      p.ellipse(distance, distance, radius, radius);
+      p.ellipse(distance, distance * -1, radius, radius);
+      p.ellipse(distance * -1, distance, radius, radius);
+      p.ellipse(0, distance, radius, radius);
+      p.ellipse(0, distance * -1, radius, radius);
+      p.ellipse(distance, 0, radius, radius);
+      p.ellipse(distance * -1, 0, radius, radius);
+      radius = radius - radiusDefault / els;
     }
   };
 
@@ -46,7 +40,9 @@ const sketch = p => {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 
-  p.keyPressed = () => {};
+  p.mousePressed = () => {
+    distance -= 1;
+  };
 };
 
 new p5(sketch);
